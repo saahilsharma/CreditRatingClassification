cd5f6112e5519497505f5ff792eb1274dceebb3d
Saahil Sarma <saahil.sarma@met.com>
2020-11-25 10:16:09 +0000

    Added yahoo_fin package

--- DataVis.py
+++ DataVis.py
@@ -2,10 +2,12 @@
 import numpy as np
 import pandas as pd
 import xlrd
+import yahoo_fin.stock_info as si
 
 # Visualization
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pandas import DataFrame
 plt.style.use('seaborn-whitegrid')
 
 # Preprocessing
@@ -23,27 +25,35 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_predict
 
-not_junk_rating = ['A','A-','A *-','A- *','A- *-','A+','AA','AA-','AA- *-','AA+','AAA','BBB','BBB-','BBB- *-',
-'BBB *+','BBB- *+','BBB+','BBB+ *-','BBB+ *+','BBBu','BBB-u']
+def fillInNaColumnsFromYahooFinance(df) -> DataFrame:
 
-sp_1500 = pd.read_excel('S&P1500_Data.xlsx')
-sp_1500.drop(['Weight', 'Shares', 'Price','RTG_FITCH_SEN_UNSECURED',
-       'RTG_MOODY_LONG_TERM', 'ESG_RATING', 'INT_COVERAGE_RATIO'], axis=1, inplace=True)
+       return df
 
-sp_1500.dropna(subset=['RTG_SP_LT_LC_ISSUER_CREDIT'], inplace=True)
-sp_1500_clean = sp_1500[~sp_1500['RTG_SP_LT_LC_ISSUER_CREDIT'].isin(['NR','SD'])]
-sp_1500_clean['OurRating'] = sp_1500_clean['RTG_SP_LT_LC_ISSUER_CREDIT'].apply(lambda x: 'Not Junk' if(x in not_junk_rating) else 'Junk')
 
-print(len(sp_1500_clean.dropna()))
+if __name__ == "__main__":
+       not_junk_rating = ['A','A-','A *-','A- *','A- *-','A+','AA','AA-','AA- *-','AA+','AAA','BBB','BBB-','BBB- *-',
+       'BBB *+','BBB- *+','BBB+','BBB+ *-','BBB+ *+','BBBu','BBB-u']
 
-plt.scatter(sp_1500_clean['CF_FREE_CASH_FLOW'], sp_1500_clean['OurRating'], c='g', s=4)
-plt.show()
-plt.scatter(sp_1500_clean['BEST_EPS'], sp_1500_clean['OurRating'], c='g', s=4)
-plt.show()
-plt.scatter(sp_1500_clean['RETURN_ON_ASSET'], sp_1500_clean['OurRating'], c='g', s=4)
-plt.show()
-plt.scatter(sp_1500_clean['EBIT'], sp_1500_clean['OurRating'], c='g', s=4)
-plt.show()
+       sp_1500 = pd.read_excel('S&P1500_Data.xlsx')
+       sp_1500.drop(['Weight', 'Shares', 'Price','RTG_FITCH_SEN_UNSECURED',
+              'RTG_MOODY_LONG_TERM', 'ESG_RATING', 'INT_COVERAGE_RATIO'], axis=1, inplace=True)
+
+       sp_1500.dropna(subset=['RTG_SP_LT_LC_ISSUER_CREDIT'], inplace=True)
+       sp_1500_clean = sp_1500[~sp_1500['RTG_SP_LT_LC_ISSUER_CREDIT'].isin(['NR','SD'])]
+       sp_1500_clean['OurRating'] = sp_1500_clean['RTG_SP_LT_LC_ISSUER_CREDIT'].apply(lambda x: 'Not Junk' if(x in not_junk_rating) else 'Junk')
+
+       #quote = si.get_quote_table(sp_1500_clean['Ticker'][0])
+       fin = si.get_financials("PFG")
+       print(fin)
+
+# plt.scatter(sp_1500_clean['CF_FREE_CASH_FLOW'], sp_1500_clean['OurRating'], c='g', s=4)
+# plt.show()
+# plt.scatter(sp_1500_clean['BEST_EPS'], sp_1500_clean['OurRating'], c='g', s=4)
+# plt.show()
+# plt.scatter(sp_1500_clean['RETURN_ON_ASSET'], sp_1500_clean['OurRating'], c='g', s=4)
+# plt.show()
+# plt.scatter(sp_1500_clean['EBIT'], sp_1500_clean['OurRating'], c='g', s=4)
+# plt.show()
 
 #TODO: Plot a few more scatter plots to see the relationships
 #TODO: Decide on approach for classification
